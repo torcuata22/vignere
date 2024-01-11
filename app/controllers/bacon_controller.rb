172b978efe9e5816e06d67e8bc65de5c19459cb6
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-01-10 22:25:58 -0500

    need to debug

--- app/controllers/bacon_controller.rb
+++ app/controllers/bacon_controller.rb
@@ -1,24 +1,77 @@
-class BaconCOntroller < ApplicationController
+class BaconController < ApplicationController
   def index
-
   end
 
   def encode
+    @bacon = Bacon.new
+    @original_text = params[:bacon][:to_encode].upcase if params[:bacon].present?
+
+    if @original_text.present?
+      @bacon.to_encode = @original_text
+      @bacon.to_decode = perform_encoding(@original_text)
+      @encoded_text = @bacon.to_decode
+      puts @encoded_text
+      render :encode
+    end
 
   end
 
   def decode
+    @bacon = Bacon.new
+    @encoded_text = params[:bacon][:to_decode] if params[:bacon].present?
 
+    puts "Params received: #{params.inspect}"
+    puts "Encoded text: #{@encoded_text}"
+
+    if @encoded_text.present?
+      @bacon.to_decode = @encoded_text
+      puts "encoded text: #{@bacon.to_decode}"
+      @decoded_text = perform_decoding(@encoded_text)
+      puts "Decoded text: #{@decoded_text}"  # Print to console
+    end
+
+    render :decode
   end
+
 
   private
 
-  def perform_encoding()
+  def perform_encoding(original_text)
+    bacon_map = {
+      'A' => '00000', 'B' => '00001', 'C' => '00010', 'D' => '00011',
+      'E' => '00100', 'F' => '00101', 'G' => '00110', 'H' => '00111',
+      'I' => '01000', 'J' => '01001', 'K' => '01010', 'L' => '01011',
+      'M' => '01100', 'N' => '01101', 'O' => '01110', 'P' => '01111',
+      'Q' => '10000', 'R' => '10001', 'S' => '10010', 'T' => '10011',
+      'U' => '10100', 'V' => '10101', 'W' => '10110', 'X' => '10111',
+      'Y' => '11000', 'Z' => '11001'
+    }
 
+    encoded_text = original_text.chars.map { |char| bacon_map[char] || char }.join
+    encoded_text
   end
 
-  def perform_decoding()
 
+  def perform_decoding(encoded_text)
+    puts "performing decoding on this text: #{encoded_text}"
+
+    bacon_map = {
+      'A' => '00000', 'B' => '00001', 'C' => '00010', 'D' => '00011',
+      'E' => '00100', 'F' => '00101', 'G' => '00110', 'H' => '00111',
+      'I' => '01000', 'J' => '01001', 'K' => '01010', 'L' => '01011',
+      'M' => '01100', 'N' => '01101', 'O' => '01110', 'P' => '01111',
+      'Q' => '10000', 'R' => '10001', 'S' => '10010', 'T' => '10011',
+      'U' => '10100', 'V' => '10101', 'W' => '10110', 'X' => '10111',
+      'Y' => '11000', 'Z' => '11001'
+    }
+
+    decoded_segments = encoded_text.scan(/.{5}/)
+    puts "Decoded segments: #{decoded_segments}"
+
+  decoded_text = decoded_segments.map { |segment| bacon_map[segment] || segment }.join
+  puts "Decoded text: #{decoded_text}"
+  decoded_text
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,15 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :bacon, only: [:index]  do
+    collection do
+      get :encode
+      post :encode
+      get :decode
+      post :decode
+    end
+  end
+
 
   # get 'login', to: 'sessions#new'
   # post 'login', to: 'sessions#create'
