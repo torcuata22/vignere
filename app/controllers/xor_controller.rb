9d3db7bca31764bd814fcd31275a73f700df6fd8
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-01-11 22:33:43 -0500

    xor encoding done

--- app/controllers/xor_controller.rb
+++ app/controllers/xor_controller.rb
@@ -15,6 +15,11 @@ class XorController < ApplicationController
       puts @encoded_text
       render :encode
     end
+    puts "Original Text: #{@original_text}"
+    puts "Key: #{@xor.key}"
+    puts "Encoded Text: #{@encoded_text}"
+
+    @encoded_text
   end
 
   def decode
@@ -24,14 +29,25 @@ class XorController < ApplicationController
   private
 
   def perform_encoding(original_text, key)
+    key = key * (original_text.length / key.length) + key[0, original_text.length % key.length]
     encoded_text = ""
 
     original_text.each_char do |char|
-    char_binary = char.ord.to_s(2).rjust(8,'0')
-    encoded_char_binary = char_binary.chars.zip(key.chars).map { |a, b| (a.to_i ^ b.to_i).to_s }.join
-    encoded_char = encoded_char_binary.to_i(2).chr
-    encoded_text += encoded_char
+      char_binary = char.ord.to_s(2).rjust(8,'0')
+      puts "char_binary: #{char_binary}"
+
+      key_char = key[0]
+      key = key[1..-1] + key_char
+      key_binary = key_char.ord.to_s(2).rjust(8,'0')
+      puts "key_binary: #{key_binary}"
+
+      encoded_char_binary = char_binary.chars.zip(key_binary.chars).map { |a, b| (a.to_i ^ b.to_i).to_s }.join
+      puts "encoded_char_binary: #{encoded_char_binary}"
+
+      encoded_char = encoded_char_binary.to_i(2).chr
+      encoded_text += encoded_char
     end
+
     encoded_text
   end
 
