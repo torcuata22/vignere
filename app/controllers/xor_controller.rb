7ff3108144937167c6c67c408fbb42d26dcb0bb4
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-01-13 15:23:27 -0500

    bugs one and two solved, how many more to go?

--- app/controllers/xor_controller.rb
+++ app/controllers/xor_controller.rb
@@ -23,17 +23,19 @@ class XorController < ApplicationController
   end
 
   def decode
-    @vign = Xor.new
-    encoded_text = params.dig(:xor, :to_decode)
+    @xor = Xor.new
+    @encoded_text = params.dig(:xor, :to_decode)
     key = params.dig(:xor, :key)
 
-    if encoded_text.present?
-      @xor.to_decode = perform_decoding(encoded_text, key)
+    if @encoded_text.present?
+      @xor.to_decode = perform_decoding(@encoded_text, key)
       @decoded_text = @xor.to_decode
       flash[:success] = 'Text decoded successfully!'
+      puts "Received encoded text: #{@encoded_text}"
+      puts "Received key: #{key}"
+      puts "Decoded text: #{@decoded_text}" # Add this line
       render :decode
     else
-
       render :decode
     end
   end
@@ -59,20 +61,38 @@ class XorController < ApplicationController
 
 
   def perform_decoding(encoded_text, key)
-    key = key * (original_text.length / key.length) + key[0, original_text.length % key.length]
+    # key = key * (encoded_text.length / key.length) + key[0, encoded_text.length % key.length]
+    key = key * (encoded_text.length / key.length + 1)
+    puts "Received encoded text: #{encoded_text}"
+    puts "Received key: #{key}"
+
     decoded_text = ""
 
     encoded_text.each_char.with_index do |char, index|
+      next if char == ' '
       char_binary = char.ord.to_s(2).rjust(8,'0')
       key_char = key[index]
       key_binary = key_char.ord.to_s(2).rjust(8, '0')
 
-      decoded_char_binary = char_binary.chars.zip(key_binary.chars).map { |a,b| (a.to_i^b.to_i).to_s }.join
-      decoded_char = decoded_char_binary.to_i(3).chr
+      puts "char: #{char}"
+      puts "char_binary: #{char_binary}"
+      puts "key_char: #{key_char}"
+      puts "key_binary: #{key_binary}"
+
+      decoded_char_binary = char_binary.chars.zip(key_binary.chars).map { |a, b| (a.to_i ^ b.to_i).to_s }.join
+      puts "decoded_char_binary: #{decoded_char_binary}"
+
+      decoded_char = decoded_char_binary.to_i(2).chr
+      puts "decoded_char: #{decoded_char}"
 
       decoded_text += decoded_char
     end
+
+    puts "Decoded text: #{decoded_text}"
     decoded_text
   end
+
+
+
 
 end
