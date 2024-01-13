be203198b78a5ac9c0dd801e806ba922d0d526c9
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-01-13 14:31:49 -0500

    started decoding xor

--- app/controllers/xor_controller.rb
+++ app/controllers/xor_controller.rb
@@ -23,7 +23,19 @@ class XorController < ApplicationController
   end
 
   def decode
+    @vign = Xor.new
+    encoded_text = params.dig(:xor, :to_decode)
+    key = params.dig(:xor, :key)
 
+    if encoded_text.present?
+      @xor.to_decode = perform_decoding(encoded_text, key)
+      @decoded_text = @xor.to_decode
+      flash[:success] = 'Text decoded successfully!'
+      render :decode
+    else
+
+      render :decode
+    end
   end
 
   private
@@ -32,18 +44,12 @@ class XorController < ApplicationController
     key = key * (original_text.length / key.length) + key[0, original_text.length % key.length]
     encoded_text = ""
 
-    original_text.each_char do |char|
+    original_text.each_char.with_index do |char, index|
       char_binary = char.ord.to_s(2).rjust(8,'0')
-      puts "char_binary: #{char_binary}"
-
-      key_char = key[0]
-      key = key[1..-1] + key_char
+      key_char = key[index]
       key_binary = key_char.ord.to_s(2).rjust(8,'0')
-      puts "key_binary: #{key_binary}"
 
       encoded_char_binary = char_binary.chars.zip(key_binary.chars).map { |a, b| (a.to_i ^ b.to_i).to_s }.join
-      puts "encoded_char_binary: #{encoded_char_binary}"
-
       encoded_char = encoded_char_binary.to_i(2).chr
       encoded_text += encoded_char
     end
@@ -52,8 +58,21 @@ class XorController < ApplicationController
   end
 
 
-  def perform_decoding()
+  def perform_decoding(encoded_text, key)
+    key = key * (original_text.length / key.length) + key[0, original_text.length % key.length]
+    decoded_text = ""
 
+    encoded_text.each_char.with_index do |char, index|
+      char_binary = char.ord.to_s(2).rjust(8,'0')
+      key_char = key[index]
+      key_binary = key_char.ord.to_s(2).rjust(8, '0')
+
+      decoded_char_binary = char_binary.chars.zip(key_binary.chars).map { |a,b| (a.to_i^b.to_i).to_s }.join
+      decoded_char = decoded_char_binary.to_i(3).chr
+
+      decoded_text += decoded_char
+    end
+    decoded_text
   end
 
 end
