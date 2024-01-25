a1cc289469e207db4b6c7c0c118e4a5913b23021
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-01-25 13:17:21 -0500

    my messages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,15 @@
 class WelcomeController < ApplicationController
 
   def index
-    
+
   end
 
   def about
-
+    @user = current_user
+    @bacons = @user.bacons
+    @caesars = @user.caesars
+    @vigns = @user.vigns
+    @xors = @user.xors
   end
-  
+
 end
