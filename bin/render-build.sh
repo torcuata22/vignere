27e89c1f3f00bc8157c0a22f0d931a0dc8bcd674
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-02-03 16:54:22 -0500

    restarting

--- bin/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-chmod +x ./bin/rails  # Add this line to set execute permissions for ./bin/rails
-
-bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
